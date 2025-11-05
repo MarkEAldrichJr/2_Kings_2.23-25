cf628c27ad65a267d9ff21609941643799c1cad9
Mark Aldrich <44414229+MarkEAldrichJr@users.noreply.github.com>
2025-11-05 10:06:51 -0500

    Added KillMainCameraWhenPlayersJoin script.  Does exactly what it says on the tin.

---
+++ Assets/Scripts/Mono/KillMainCameraWhenPlayersJoin.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Mono
+{
+    public class KillMainCameraWhenPlayersJoin : MonoBehaviour
+    {
+        [SerializeField] private GameObject cam;
+        public uint NumPlayers { private set; get; }
+        
+        private void OnPlayerJoined(PlayerInput input)
+        {
+            NumPlayers++;
+            ActivateCamera();
+        }
+
+        private void OnPlayerLeft(PlayerInput input)
+        {
+            NumPlayers--;
+            ActivateCamera();
+        }
+
+        private void ActivateCamera()
+        {
+            cam.SetActive(NumPlayers == 0);
+        }
+    }
+}
