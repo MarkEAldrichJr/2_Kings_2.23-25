054a257aad41000b150af4d7e378964cd24a65bc
Mark Aldrich <44414229+MarkEAldrichJr@users.noreply.github.com>
2025-11-20 10:52:32 -0500

    Faith slider changes color as Elisha loses faith

--- Assets/Scripts/Mono/UpdateGlobalUI.cs
+++ Assets/Scripts/Mono/UpdateGlobalUI.cs
@@ -1,6 +1,8 @@
-﻿using Component;
+﻿using System;
+using Component;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,24 +10,58 @@ namespace Mono
 {
     public class UpdateGlobalUI : MonoBehaviour
     {
+        [Header("Faith Slider")]
         [SerializeField] private Slider faithSlider;
+        [SerializeField] private Image sliderImage;
+        [SerializeField] private Color fullColor = Color.yellowNice;
+        [SerializeField] private Color emptyColor = Color.grey;
+        
         private EntityQuery _entityQuery;
         private float _faithLastFrame;
+        private Gradient _faithColorGradient;
+
+        private void Awake()
+        {
+            SetFaithColorGradient();
+        }
+
         
+
         private void Start()
         {
             var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-            var builder = new EntityQueryBuilder(Allocator.Temp).WithAll<ElishaFaith>();
+            var builder = new EntityQueryBuilder(Allocator.Temp).WithAll<ElishaFaith, LocalTransform>();
             _entityQuery = entityManager.CreateEntityQuery(builder);
         }
 
         private void LateUpdate()
+        {
+            UpdateFaithSlider();
+        }
+        
+        private void SetFaithColorGradient()
+        {
+            _faithColorGradient = new Gradient();
+            var gck = new GradientColorKey[2];
+            gck[0].color = emptyColor;
+            gck[0].time = 0.0f;
+            gck[1].color = fullColor;
+            gck[1].time = 1.0f;
+            
+            var gak = Array.Empty<GradientAlphaKey>();
+            
+            _faithColorGradient.SetKeys(gck, gak);
+        }
+        private void UpdateFaithSlider()
         {
             var faith = _entityQuery.GetSingleton<ElishaFaith>();
             var faithRatio = faith.CurrentFaith / faith.FaithMax;
             if (Mathf.Approximately(_faithLastFrame, faithRatio)) return;
             
             faithSlider.value = faithRatio;
+
+            sliderImage.color = _faithColorGradient.Evaluate(faithRatio);
+            
             _faithLastFrame = faithRatio;
         }
     }
