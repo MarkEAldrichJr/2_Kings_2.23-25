90164965e697cbe0d271e5e902800037f0da4674
Mark Aldrich <44414229+MarkEAldrichJr@users.noreply.github.com>
2026-04-06 10:21:51 -0400

    Updated Game Over screen to use gamepad and keyboard inputs instead of a button you need a mouse to select.

--- Assets/Scripts/Mono/RestartGame.cs
+++ Assets/Scripts/Mono/RestartGame.cs
@@ -1,25 +1,88 @@
+using System.Globalization;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 namespace Mono
 {
     public class RestartGame : MonoBehaviour
     {
-        [SerializeField] private Button restartButton;
+        [SerializeField] private TextMeshProUGUI timerText;
+        [SerializeField] private TextMeshProUGUI promptText;
+        [SerializeField] private float timer;
+
+        private float _timeLeft;
+        private string _timeWritten;
+        private string _timeLastFrame;
+        private bool _isReadyForInput;
 
         private void Awake()
         {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
+            _timeLeft = timer;
+            _isReadyForInput = false;
+            timerText.text = timer.ToString(CultureInfo.InvariantCulture);
+
+            promptText.gameObject.SetActive(false);
+            timerText.gameObject.SetActive(true);
         }
 
-        private void OnEnable() => restartButton.onClick.AddListener(LoadFirstScene);
-        private void OnDisable() => restartButton.onClick.RemoveListener(LoadFirstScene);
-        
-        private static void LoadFirstScene()
+        private void Update()
         {
-            SceneManager.LoadScene(0);
+            if (CheckReadyForInput())
+                ActOnInput();
         }
+
+        private bool CheckReadyForInput()
+        {
+            if (!_isReadyForInput)
+            {
+                _timeLeft -= Time.deltaTime;
+                _timeWritten = math.round(_timeLeft).ToString(CultureInfo.InvariantCulture);
+                
+                if (_timeWritten != _timeLastFrame)
+                {
+                    timerText.text = _timeWritten;
+                    _timeLastFrame = _timeWritten;
+                }
+
+                if (_timeLeft > 0)
+                {
+                    _isReadyForInput = false;
+                    return false;
+                }
+
+                _isReadyForInput = true;
+                promptText.gameObject.SetActive(true);
+                timerText.gameObject.SetActive(false);
+            }
+            return true;
+        }
+
+        private static void ActOnInput()
+        {
+            var gamepad = Gamepad.current;
+            if (gamepad is { wasUpdatedThisFrame: true })
+            {
+                foreach (var control in gamepad.allControls)
+                {
+                    if (control is ButtonControl { wasPressedThisFrame: true })
+                    {
+                        LoadFirstScene();
+                        return;
+                    }
+                }
+            }
+            if (Keyboard.current != null &&
+                (Keyboard.current.spaceKey.wasPressedThisFrame ||
+                 Keyboard.current.enterKey.wasPressedThisFrame))
+            {
+                LoadFirstScene();
+            }
+        }
+        
+        private static void LoadFirstScene() => SceneManager.LoadScene(0);
     }
 }
